4186c877684655cab999d852720183ce78516095
Igor Kaseko <igorksk@gmail.com>
2024-08-12 21:09:49 +0300

    Small update

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -11,30 +11,39 @@ namespace FizzBuzz
             // Loop for 100 times
             for (int i = 1; i <= n; i++)
             {
-
                 // Number divisible by 15(
                 // divisible by both 3 & 5),
                 // print 'FizzBuzz' in place
                 // of the number
                 if (i % 15 == 0)
+                {
                     Console.Write("FizzBuzz" + " ");
-
+                }
+                
                 // Number divisible by 3,
                 // print 'Fizz' in place
                 // of the number
                 else if (i % 3 == 0)
+                {
                     Console.Write("Fizz" + " ");
-
+                }
+                  
                 // Number divisible by
                 // 5, print 'Buzz'
                 // in place of the number
                 else if (i % 5 == 0)
+                {
                     Console.Write("Buzz" + " ");
+                }
 
                 // Print the numbers
                 else
+                {
                     Console.Write(i + " ");
+                }    
             }
+
+            Console.ReadLine();
         }
     }
 }
